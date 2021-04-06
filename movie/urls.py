822c29fa4dbf51d7fbae59cd73d6a7c199b96bea
SonOfGrabasz <m.szul00@gmail.com>
2021-04-06 18:20:39 +0200

    commit

---
+++ movie/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from movie.views import index
+
+
+urlpatterns = [
+    path('', index, name='index'),
+]
---
+++ movie/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+import requests
+
+# Create your views here.
+
+
+def index(request):
+    query = request.GET.get('q')
+
+    if query:
+        url = 'http://www.omdbapi.com/?apikey=396fc5db&s=' + query
+        response = requests.get(url)
+        movie_data = response.json()
+
+        context = {
+            'query': query,
+            'movie_data': movie_data,
+        }
+
+        template = loader.get_template('search_results.html')
+
+        return HttpResponse(template.render(context, request))
+
+    return render(request, 'index.html')
